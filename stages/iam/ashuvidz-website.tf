caeec7295301dcdc468cbd40e2a863c5b84f2f36
Valentin RONTEIX <valentin.ronteix@pm.me>
2021-08-10 22:42:55 +0200

    WIP

---
+++ stages/iam/ashuvidz-website.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
