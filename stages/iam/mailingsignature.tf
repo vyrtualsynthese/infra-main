ea3f0361c3ff1a278b1bf9b69916512be5f361d0
Valentin RONTEIX <valentin.ronteix@pm.me>
2021-08-23 00:07:21 +0200

    :construction: s3 rights managed

--- stages/iam/mailingsignature.tf
+++ stages/iam/mailingsignature.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_policy" "policy" {
+resource "aws_iam_policy" "mailingsignature" {
   name        = "mailingsignature"
   path        = "/projects/"
   tags        = {
@@ -12,13 +12,54 @@ resource "aws_iam_policy" "policy" {
           "s3:*",
         ]
         Effect   = "Allow"
-        Resource = "*"
-        Condition : {
-          "StringEquals" : {
-            "s3:ResourceTag/project": "mailingsignature"
-          }
-        }
+        Resource = "arn:aws:s3:::mailingsignature"
       },
     ]
   })
+}
+
+resource "aws_iam_user" "mailingsignature-terraform" {
+  name = "mailingsignature-terraform"
+  path = "/projects/mailingsignature/"
+
+  tags = {
+    project = "mailingsignature"
+  }
+}
+
+resource "aws_iam_access_key" "mailingsignature-terraform" {
+  user    = aws_iam_user.mailingsignature-terraform.name
+}
+
+resource "aws_iam_group" "mailingsignature" {
+  name = "mailingsignature"
+  path = "/projects/mailingsignature/"
+}
+
+resource "aws_iam_group_membership" "mailingsignature" {
+  name = "tf-mailingsignature-group-membership"
+
+  users = [
+    aws_iam_user.mailingsignature-terraform.name,
+  ]
+
+  group = aws_iam_group.mailingsignature.name
+}
+
+resource "aws_iam_group_policy_attachment" "mailingsignature" {
+  group      = aws_iam_group.mailingsignature.name
+  policy_arn = aws_iam_policy.mailingsignature.arn
+}
+
+# + Attach policy to than group
+
+
+# Should find a way tu push those variables to github action later
+output "access" {
+  value = aws_iam_access_key.mailingsignature-terraform.id
+}
+
+output "secret" {
+  value = aws_iam_access_key.mailingsignature-terraform.secret
+  sensitive = true
 }
