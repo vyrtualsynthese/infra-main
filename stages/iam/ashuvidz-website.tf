d39343af7f702812cf44683ef389e30c7747f7d7
Valentin RONTEIX <valentin.ronteix@pm.me>
2021-08-22 17:29:21 +0200

    :sparkles: feat: implemented aws support

--- stages/iam/ashuvidz-website.tf
+++ stages/iam/ashuvidz-website.tf
@@ -2,20 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 1.0"
+      version = "~> 3.55"
     }
   }
 }
 
 provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
 }
