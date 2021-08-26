672f6a73430ac3af908128fa231d4aa0d5b5c592
Valentin RONTEIX <valentin.ronteix@ashudev.com>
2021-08-26 22:50:37 +0200

    :construction_worker: update ci for infraMain

---
+++ infraMain/budget.tf
@@ -0,0 +1,34 @@
+terraform {
+
+  backend "s3" {
+    encrypt        = true
+    bucket         = "ashudev-tf-states"
+    key            = "projects/budget.tfstate"
+    dynamodb_table = "tf-main-lock"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.55"
+    }
+  }
+}
+
+resource "aws_budgets_budget" "all" {
+  name              = "Monthly budget"
+  budget_type       = "COST"
+  limit_amount      = "5"
+  limit_unit        = "USD"
+  time_period_end   = "2087-06-15_00:00"
+  time_period_start = "2017-07-01_00:00"
+  time_unit         = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["valentin.ronteix@ashudev.com"]
+  }
+}
